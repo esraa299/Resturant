4fa037ee060f11b734e04078bcf12d1d1280b076
esraa299 <esraasaeed342@gmail.com>
2025-04-28 23:01:52 -0700

    version 0.1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,75 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './features/pages/hero/hero.component';
+import { AboutComponent } from './features/pages/about/about.component';
+import { BookingComponent } from './features/pages/booking/booking.component';
+import { ChefsComponent } from './features/pages/chefs/chefs.component';
+import { ContactComponent } from './features/pages/contact/contact.component';
+import { EventsComponent } from './features/pages/events/events.component';
+import { GalleryComponent } from './features/pages/gallery/gallery.component';
+import { MenuComponent } from './features/pages/menu/menu.component';
+import { StatsComponent } from './features/pages/stats/stats.component';
+import { TestimonialsComponent } from './features/pages/testimonials/testimonials.component';
+import { WhyusComponent } from './features/pages/whyus/whyus.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:HeroComponent,
+        title:"Mr. Burger | Home"
+    },
+    {
+        path:"**",
+        redirectTo:'',
+        pathMatch:'full'
+    },
+    {
+        path:"about",
+        component:AboutComponent,
+        title:"Mr. Burger | About"
+    },
+    {
+        path:"booking",
+        component:BookingComponent,
+        title:"Mr. Burger | Booking"
+    },
+    {
+        path:"chefs",
+        component:ChefsComponent,
+        title:"Mr. Burger | Chefs"
+    },
+    {
+        path:"contact",
+        component:ContactComponent,
+        title:"Mr. Burger | contact"
+    },
+    {
+        path:"events",
+        component:EventsComponent,
+        title:"Mr. Burger | events"
+    },
+    {
+        path:"gallery",
+        component:GalleryComponent,
+        title:"Mr. Burger | Gallery"
+    },
+    {
+        path:"menu",
+        component:MenuComponent,
+        title:"Mr. Burger | Menu"
+    },
+    {
+        path:"Stats",
+        component:StatsComponent,
+        title:"Mr. Burger | Stats"
+    },
+    {
+        path:"testimonials",
+        component:TestimonialsComponent,
+        title:"Mr. Burger | Testimonials"
+    },
+    {
+        path:"whyus",
+        component:WhyusComponent,
+        title:"Mr. Burger | WhyUs"
+    }
+];
